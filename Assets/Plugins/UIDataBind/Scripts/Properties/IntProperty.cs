2393e92e557a1c6af5a5dd69bd407a2b1c90a06f
Andrew Salomatin <merlinkolv@gmail.com>
2019-06-23 19:23:28 +0300

    Implemented int property

---
+++ Assets/Plugins/UIDataBind/Scripts/Properties/IntProperty.cs
@@ -0,0 +1,9 @@
+namespace Plugins.UIDataBind.Properties
+{
+    public sealed class IntProperty : BaseBindingProperty<int>
+    {
+        public IntProperty(int value = default) : base(value)
+        {
+        }
+    }
+}
