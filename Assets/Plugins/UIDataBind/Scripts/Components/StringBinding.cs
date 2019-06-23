0c8e555b752c245b6c2fdfa9fe80e022ea6e1769
Andrew Salomatin <merlinkolv@gmail.com>
2019-06-23 19:15:42 +0300

    Implemented string binding for Text Mesh Pro component

---
+++ Assets/Plugins/UIDataBind/Scripts/Components/StringBinding.cs
@@ -0,0 +1,19 @@
+using TMPro;
+using UnityEngine;
+
+namespace Plugins.UIDataBind.Components
+{
+    [AddComponentMenu("UIDataBind/TMP - String", 1)]
+    [RequireComponent(typeof(TextMeshProUGUI))]
+    public class StringBinding : BasePropertyBindingWithComponentBehaviour<TextMeshProUGUI, string>
+    {
+        [SerializeField]
+        private string _format = "{0}";
+        protected override void UpdateValueHandler(string value)
+        {
+            if (!string.IsNullOrEmpty(_format))
+                value = string.Format(_format, value);
+            Component.text = value;
+        }
+    }
+}
