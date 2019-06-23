70c0b5a57c9d2b44bae2ecb61df043b1e0ebcaeb
Andrew Salomatin <merlinkolv@gmail.com>
2019-06-23 19:20:16 +0300

    Implemented stringProperty

---
+++ Assets/Plugins/UIDataBind/Scripts/Properties/StringProperty.cs
@@ -0,0 +1,9 @@
+namespace Plugins.UIDataBind.Properties
+{
+    public class StringProperty : BaseBindingProperty<string>
+    {
+        public StringProperty(string value = default) : base(value)
+        {
+        }
+    }
+}
