a4558d61abd070c460806643d2a504f869a16e8c
Andrew Salomatin <merlinkolv@gmail.com>
2019-06-23 15:26:18 +0300

    Implemented methods for changing of a visibility

--- Assets/Plugins/UIDataBind/Scripts/Components/VisibleBinding.cs
+++ Assets/Plugins/UIDataBind/Scripts/Components/VisibleBinding.cs
@@ -1,4 +1,6 @@
+using System.Collections;
 using System.Collections.Generic;
+using JetBrains.Annotations;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -7,56 +9,71 @@ namespace Plugins.UIDataBind.Components
     [AddComponentMenu("UIDataBind/Visible", 0)]
     public sealed class VisibleBinding : BasePropertyBindingBehaviour<bool>
     {
-        private readonly List<Graphic> _graphicsCache = new List<Graphic>();
-        private readonly List<LayoutElement> _layoutCache = new List<LayoutElement>();
-        private readonly List<VisibleBinding> _visibleCache = new List<VisibleBinding>();
+        private readonly HashSet<Graphic> _graphics = new HashSet<Graphic>();
+        private readonly HashSet<LayoutElement> _layouts = new HashSet<LayoutElement>();
+        private readonly HashSet<VisibleBinding> _binders = new HashSet<VisibleBinding>();
 
-        private readonly Queue<Transform> _transforms = new Queue<Transform>();
+        private readonly Queue<Transform> _transformsBuffer = new Queue<Transform>();
+        private bool _isDirty = true;
+
 
         protected override void UpdateValueHandler(bool value)
         {
-            UpdateCache();
+            if (_isDirty)
+                UpdateCache();
 
-            foreach (var graphic in _graphicsCache)
+            foreach (var graphic in _graphics)
                 graphic.enabled = value;
 
-            foreach (var layout in _layoutCache)
+            foreach (var layout in _layouts)
                 layout.ignoreLayout = !value;
 
-            foreach (var binding in _visibleCache)
-                binding.Value = value;
         }
 
         private void UpdateCache()
         {
-            _graphicsCache.Clear();
-            _visibleCache.Clear();
-            _layoutCache.Clear();
-
-            _transforms.Enqueue(transform);
-            while (_transforms.Count > 0)
+            _isDirty = false;
+            _transformsBuffer.Enqueue(transform);
+            while (_transformsBuffer.Count > 0)
             {
-                var component = _transforms.Dequeue();
-                foreach (Component child in component)
-                    _transforms.Enqueue((Transform) child);
+                var component = _transformsBuffer.Dequeue();
+                AddChildren(component, _transformsBuffer);
+                if(component != transform)
+                    CollectComponents(component, _binders);
 
-                var visibleBinder = component.GetComponent<VisibleBinding>();
-                if (!visibleBinder || !visibleBinder.enabled || component == transform)
-                {
-                    _graphicsCache.AddRange(GetAllGraphicComponents(component));
-                    _layoutCache.AddRange(GetAllLayoutElementComponents(component));
-                    continue;
-                }
-
-                _visibleCache.Add(visibleBinder);
-
+                CollectComponents(component, _graphics);
+                CollectComponents(component, _layouts);
             }
         }
-        private static IEnumerable<Graphic> GetAllGraphicComponents(Component component) =>
-            component.GetComponents<Graphic>();
 
-        private static IEnumerable<LayoutElement> GetAllLayoutElementComponents(Component component) =>
-            component.GetComponents<LayoutElement>();
+        private static void AddChildren(IEnumerable parent, Queue<Transform> buffer)
+        {
+            foreach (Component child in parent)
+                buffer.Enqueue((Transform) child);
+        }
+
+        private static void CollectComponents<TComponent>(Component container, ICollection<TComponent> cacheBuffer,
+            bool onlyEnabled = false) where TComponent : Behaviour
+        {
+            if(container == null)
+                return;
+
+            var components = container.GetComponents<TComponent>();
+            foreach (var component in components)
+            {
+                if(!onlyEnabled || component.enabled)
+                    cacheBuffer.Add(component);
+            }
+        }
+
+        [UsedImplicitly]
+        private void CleanCache()
+        {
+            _graphics.Clear();
+            _binders.Clear();
+            _layouts.Clear();
+            _isDirty = true;
+        }
 
 #if UNITY_EDITOR
         [ContextMenu("ChangeVisibility")]
