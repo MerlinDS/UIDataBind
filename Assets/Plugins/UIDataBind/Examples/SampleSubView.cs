fb370ecca2f36c46b6552148fdbe61e3f4f1000c
Andrew Salomatin <merlinkolv@gmail.com>
2019-07-01 18:55:50 +0300

    Update samples

--- Assets/Plugins/UIDataBind/Examples/SampleSubView.cs
+++ Assets/Plugins/UIDataBind/Examples/SampleSubView.cs
@@ -1,9 +1,63 @@
+using JetBrains.Annotations;
+using Plugins.UIDataBind.Attributes;
+using Plugins.UIDataBind.Properties;
 using UnityEngine;
 
 namespace Plugins.UIDataBind.Examples
 {
     public class SampleSubView : MonoBehaviour, IViewContext
     {
+        [UsedImplicitly, BindingMethod("Image Visibility Change")]
+        private void OnImageVisibilityChange() =>
+            ImageVisible = !ImageVisible;
 
+        [UsedImplicitly,BindingMethod("Image Color Change")]
+        private void OnImageColorChange() =>
+            ImageColor = ImageColor == Color.white ? Color.red : Color.white;
+
+        [UsedImplicitly,BindingMethod("Image Label Change")]
+        private void OnImageLabelChange() =>
+            ImageValue++;
+
+        #region Properties
+
+        [BindingProperty]
+        private readonly BooleanProperty _visibleProperty = new BooleanProperty(true);
+
+        public bool Visible
+        {
+            get => _visibleProperty.Value;
+            set => _visibleProperty.Value = value;
+        }
+
+        [BindingProperty("Plugin Image Visibility")]
+        private readonly BooleanProperty _imageVisibleProperty = new BooleanProperty(true);
+
+        public bool ImageVisible
+        {
+            get => _imageVisibleProperty.Value;
+            set => _imageVisibleProperty.Value = value;
+        }
+
+
+        [BindingProperty("Plugin Image Color")]
+        private readonly ColorProperty _imageColorProperty = new ColorProperty(Color.white);
+
+        public Color ImageColor
+        {
+            get => _imageColorProperty.Value;
+            set => _imageColorProperty.Value = value;
+        }
+
+        [BindingProperty("Plugin Image Value")]
+        private readonly IntProperty _imageLabelProperty = new IntProperty();
+
+        public int ImageValue
+        {
+            get => _imageLabelProperty.Value;
+            set => _imageLabelProperty.Value = value;
+        }
+
+        #endregion
     }
 }
--- Assets/Plugins/UIDataBind/Examples/SampleView.cs
+++ Assets/Plugins/UIDataBind/Examples/SampleView.cs
@@ -1,3 +1,4 @@
+using JetBrains.Annotations;
 using Plugins.UIDataBind.Attributes;
 using Plugins.UIDataBind.Properties;
 using UnityEngine;
@@ -6,30 +7,9 @@ namespace Plugins.UIDataBind.Examples
 {
     public class SampleView : MonoBehaviour, IViewContext
     {
-#pragma warning disable 0649
-        [SerializeField]
-        private Sprite _testSprite;
-#pragma warning restore 0649
-
-        private void Start()
-        {
-            SampleSprite = _testSprite;
-            SampleSpritePath = _testSprite.name;
-            _actionProperty.OnUpdateValue += b => SwitchInt();
-        }
-
-        [BindingMethod("Sample Action")]
-        public void SwitchInt()
-        {
-            SampleInt = SampleInt > 0 ? 0 : 1;
-            SampleSpritePath = string.IsNullOrEmpty(SampleSpritePath) ? _testSprite.name : string.Empty;
-            ButtonColor = ButtonColor == Color.green ? Color.yellow : Color.green;
-        }
-
-        [BindingMethod("Sample Action 2")]
-        public void SwitchVisibility() =>
+        [UsedImplicitly, BindingMethod("Visibility Change")]
+        private void OnVisibilityChanged() =>
             Visible = !Visible;
-
 
         #region Properties
 
@@ -41,72 +21,6 @@ namespace Plugins.UIDataBind.Examples
             get => _visibleProperty.Value;
             set => _visibleProperty.Value = value;
         }
-
-        [BindingProperty("ImageVisible")]
-        private readonly BooleanProperty _sampleImageVisibleProperty = new BooleanProperty(true);
-
-        public bool SampleImageVisible
-        {
-            get => _sampleImageVisibleProperty.Value;
-            set => _sampleImageVisibleProperty.Value = value;
-        }
-
-        [BindingProperty]
-        private readonly StringProperty _sampleStringProperty = new StringProperty("Some sample string from code");
-
-        public string SampleString
-        {
-            get => _sampleStringProperty.Value;
-            set => _sampleStringProperty.Value = value;
-        }
-
-        [BindingProperty]
-        private readonly IntProperty _sampleIntProperty = new IntProperty(10);
-
-        public int SampleInt
-        {
-            get => _sampleIntProperty.Value;
-            set => _sampleIntProperty.Value = value;
-        }
-
-        [BindingProperty]
-        private readonly SpriteProperty _sampleSpriteProperty = new SpriteProperty();
-
-        public Sprite SampleSprite
-        {
-            get => _sampleSpriteProperty.Value;
-            set => _sampleSpriteProperty.Value = value;
-        }
-
-        [BindingProperty]
-        private readonly StringProperty _sampleSpritePathProperty = new StringProperty();
-
-        public string SampleSpritePath
-        {
-            get => _sampleSpritePathProperty.Value;
-            set => _sampleSpritePathProperty.Value = value;
-        }
-
-        [BindingProperty("Button Label")]
-        private readonly StringProperty _buttonProperty = new StringProperty("Visible off");
-
-        public string ButtonLabel
-        {
-            get => _buttonProperty.Value;
-            set => _buttonProperty.Value = value;
-        }
-
-        [BindingProperty("Button Color")]
-        private readonly ColorProperty _buttonColorProperty = new ColorProperty(Color.green);
-
-        public Color ButtonColor
-        {
-            get => _buttonColorProperty.Value;
-            set => _buttonColorProperty.Value = value;
-        }
-
-        [BindingProperty("Action")]
-        private readonly BooleanProperty _actionProperty = new BooleanProperty();
 
         #endregion
     }
