5d613cbd4be8d8bf2e30c6839c3f918068f7a3af
Andrew Salomatin <merlinkolv@gmail.com>
2019-07-01 19:58:04 +0300

    Added summary

--- Assets/Plugins/UIDataBind/Scripts/IViewContext.cs
+++ Assets/Plugins/UIDataBind/Scripts/IViewContext.cs
@@ -1,5 +1,8 @@
 namespace Plugins.UIDataBind
 {
+    /// <summary>
+    /// A class must inherit this interface to be marked as view context.
+    /// </summary>
     public interface IViewContext
     {
 
