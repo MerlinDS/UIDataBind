6703054b8d1a4b355ae9a081d79fe5d025ca96c9
Andrew Salomatin <merlinkolv@gmail.com>
2019-07-02 23:57:35 +0300

    Implemented BaseBehavior class

---
+++ Assets/Plugins/UIDataBind/Scripts/Base/BaseBehaviour.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+
+namespace Plugins.UIDataBind.Base
+{
+    /// <summary>
+    /// Base <see cref="MonoBehaviour"/> class for all components in IUDataBind
+    /// </summary>
+    public abstract class BaseBehaviour : MonoBehaviour
+    {
+        #region Unity Events
+
+        private void OnEnable() => Activate();
+
+        private void OnDisable() => Deactivate();
+
+        #endregion
+
+        #region Abstract API
+
+        /// <summary>
+        /// Activation actions of a component
+        /// </summary>
+        protected abstract void Activate();
+        /// <summary>
+        /// Deactivation actions of a component
+        /// </summary>
+        protected abstract void Deactivate();
+
+        #endregion
+
+        #region Public API
+
+        public void Reactivate()
+        {
+            Deactivate();
+            Activate();
+        }
+
+        #endregion
+    }
+}
