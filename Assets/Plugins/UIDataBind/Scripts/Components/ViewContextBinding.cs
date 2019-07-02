6ad43499c382c07c5088ecac1f3c59f3ecbdb423
Andrew Salomatin <merlinkolv@gmail.com>
2019-07-03 00:05:08 +0300

    Added class for providing IViewContext binding

---
+++ Assets/Plugins/UIDataBind/Scripts/Components/ViewContextBinding.cs
@@ -0,0 +1,17 @@
+using Plugins.UIDataBind.Base;
+
+namespace Plugins.UIDataBind.Components
+{
+    public class ViewContextBinding : BaseBinding
+    {
+        protected override void Activate(BindingPath path)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        protected override void Deactivate()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
