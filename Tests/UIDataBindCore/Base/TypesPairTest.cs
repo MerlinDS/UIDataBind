caa7e95f2f200a93cb3303440fe4ecf109bac91f
Andrew Salomatin <merlinkolv@gmail.com>
2019-08-03 16:23:52 +0300

    Implemented TypesPair for storing two types as a pair key

---
+++ Tests/UIDataBindCore/Base/TypesPairTest.cs
@@ -0,0 +1,53 @@
+using NUnit.Framework;
+using UIDataBindCore.Base;
+
+namespace Tests.UIDataBindCore.Base
+{
+    [TestFixture]
+    public class TypesPairTest
+    {
+        [Test]
+        public void AccessorsTest()
+        {
+            var typeA = typeof(int);
+            var typeB = typeof(bool);
+            var pair = new TypesPair(typeA, typeB);
+            Assert.That(pair.A, Is.SameAs(typeA));
+            Assert.That(pair.B, Is.SameAs(typeB));
+        }
+
+        [Test]
+        public void EqualsTest()
+        {
+            var a = new TypesPair(typeof(int), typeof(bool));
+            var b = new TypesPair(typeof(int), typeof(bool));
+            var c = new TypesPair( typeof(bool), typeof(int));
+            Assert.That(a.Equals(a), Is.True);
+            Assert.That(a.Equals(b), Is.True);
+            Assert.That(a.Equals(c), Is.False);
+
+            Assert.That(a.Equals((object)a), Is.True);
+            Assert.That(a.Equals((object)b), Is.True);
+            Assert.That(a.Equals((object)c), Is.False);
+        }
+
+        [Test]
+        public void GetHashCodeTest()
+        {
+            var a = new TypesPair(typeof(int), typeof(bool)).GetHashCode();
+            var b = new TypesPair(typeof(int), typeof(bool)).GetHashCode();
+            var c = new TypesPair( typeof(bool), typeof(int)).GetHashCode();
+            Assert.That(a, Is.EqualTo(a));
+            Assert.That(a, Is.EqualTo(b));
+            Assert.That(a, Is.Not.EqualTo(c));
+        }
+
+        [Test]
+        public void ToStringTest()
+        {
+            var a = new TypesPair(typeof(int), typeof(bool)).ToString();
+            var expected = $"{nameof(TypesPair)}[{typeof(int)} {typeof(bool)}]";
+            Assert.That(a, Is.EqualTo(expected));
+        }
+    }
+}
---
+++ UIDataBindCore/Sources/Base/TypesPair.cs
@@ -0,0 +1,33 @@
+using System;
+
+namespace UIDataBindCore.Base
+{
+    [Serializable]
+    public struct TypesPair
+    {
+        private readonly Type _a;
+        private readonly Type _b;
+
+        public TypesPair(Type a, Type b)
+        {
+            _a = a;
+            _b = b;
+        }
+
+        public Type A => _a;
+
+        public Type B => _b;
+
+        public bool Equals(TypesPair other) =>
+            _a == other._a && _b == other._b;
+
+        public override bool Equals(object obj) =>
+            obj is TypesPair other && Equals(other);
+
+        public override int GetHashCode() =>
+            ((_a != null ? _a.GetHashCode() : 0) * 397) ^ (_b != null ? _b.GetHashCode() : 0);
+
+        public override string ToString() =>
+            $"{nameof(TypesPair)}[{_a} {_b}]";
+    }
+}
