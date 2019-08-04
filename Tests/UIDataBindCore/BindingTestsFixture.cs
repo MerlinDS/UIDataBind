a7b0564ce3b83e63066d096ba3d965da97d53572
Andrew Salomatin <merlinkolv@gmail.com>
2019-08-04 18:20:28 +0300

    Added usage test

--- Tests/UIDataBindCore/BindingTestsFixture.cs
+++ Tests/UIDataBindCore/BindingTestsFixture.cs
@@ -1,3 +1,4 @@
+using System;
 using NUnit.Framework;
 using UIDataBindCore;
 using UIDataBindCore.Attributes;
@@ -10,13 +11,13 @@ namespace Tests.UIDataBindCore
     public class BindingTestsFixture
     {
         [Test]
-        public void Test()
+        public void BindPropertyWithSameTypeTest()
         {
             var dataContext = new DataContext();
-            var booleanBinder = new BooleanPropertyBinder();
+            var booleanBinder = new PropertyBinder<bool>(nameof(dataContext.BooleanProperty) );
             booleanBinder.Bind(dataContext);
 
-            Assert.That(booleanBinder.Property, Is.Not.Null);
+            Assert.That(booleanBinder.HasProperty, Is.True);
             Assert.That(booleanBinder.Value, Is.EqualTo(dataContext.Boolean));
             dataContext.Boolean = false;
             Assert.That(booleanBinder.Value, Is.EqualTo(dataContext.Boolean));
@@ -24,46 +25,117 @@ namespace Tests.UIDataBindCore
             Assert.That(booleanBinder.Value, Is.EqualTo(dataContext.Boolean));
             booleanBinder.Unbind();
             dataContext.Boolean = false;
-            Assert.That(booleanBinder.Property, Is.Null);
+            Assert.That(booleanBinder.HasProperty, Is.False);
 
             dataContext.Unbind();
         }
 
-        private class BooleanPropertyBinder : IBinder
+        [Test]
+        public void BindPropertyWithDiffTypeTest()
         {
-            private const string Path = "_booleanProperty";
-            public IBindProperty<bool> Property;
+            var dataContext = new DataContext();
+            var booleanBinder = new PropertyBinder<bool>(nameof(dataContext.IntProperty) );
+            booleanBinder.Bind(dataContext);
 
-            public bool Value
-            {
-                get => Property.Value;
-                set => Property.Value = value;
-            }
-            public void Bind(IDataContext context)
-            {
-                Property = context.FindProperty<bool>(Path);
-            }
+            Assert.That(booleanBinder.HasProperty, Is.True);
+            Assert.That(booleanBinder.Value, Is.EqualTo(Convert.ToBoolean(dataContext.Int32)));
+            dataContext.Int32 = 0;
+            Assert.That(booleanBinder.Value, Is.EqualTo(Convert.ToBoolean(dataContext.Int32)));
+            booleanBinder.Value = true;
+            Assert.That(booleanBinder.Value, Is.EqualTo(Convert.ToBoolean(dataContext.Int32)));
+            booleanBinder.Unbind();
+            dataContext.Int32 = 1;
+            Assert.That(booleanBinder.HasProperty, Is.False);
 
-            public void Unbind() => Property = null;
+            dataContext.Unbind();
         }
+
+        [Test]
+        public void BindMethodTest()
+        {
+            var dataContext = new DataContext();
+            var binder = new MethodBinder(nameof(dataContext.BindMethod) );
+            binder.Bind(dataContext);
+            Action expectedMethod = dataContext.BindMethod;
+            dataContext.MethodInvoked = false;
+
+            Assert.That(binder.HasMethod, Is.True);
+            Assert.That(binder.Method.Target, Is.SameAs(expectedMethod.Target));
+            binder.Invoke();
+            Assert.That(dataContext.MethodInvoked, Is.True);
+            binder.Unbind();
+            Assert.That(binder.HasMethod, Is.False);
+
+            dataContext.Unbind();
+        }
+
+        private class PropertyBinder<TValue> : IBinder
+        {
+            private readonly string _path;
+            private IBindProperty<TValue> _property;
+
+            public bool HasProperty=> _property != null;
+            public TValue Value
+            {
+                get => _property.Value;
+                set => _property.Value = value;
+            }
+            public PropertyBinder(string path) => _path = path;
+
+            public void Bind(IDataContext context) => _property = context.FindProperty<TValue>(_path);
+
+            public void Unbind() => _property = null;
+        }
+
+        private class MethodBinder : IBinder
+        {
+            private readonly string _path;
+            private Action _method;
+
+            public bool HasMethod=> _method != null;
+
+            public void Invoke() => _method.Invoke();
+
+            public MethodBinder(string path) => _path = path;
+
+            public void Bind(IDataContext context) => _method = context.FindMethod(_path);
+
+            public void Unbind() => _method = null;
+
+            public Action Method => _method;
+        }
+
         private class DataContext : IDataContext, IBinder
         {
             public DataContext() => Bind(this);
             public void Bind(IDataContext context) => context.Register();
+            public bool MethodInvoked;
 
             public void Unbind()
             {
-                _booleanProperty.Dispose();
+                BooleanProperty.Dispose();
                 this.Unregister();
             }
 
             [Bind]
-            private readonly BindProperty<bool> _booleanProperty = new BindProperty<bool>(true);
+            public void BindMethod() => MethodInvoked = true;
+
+            [Bind]
+            public readonly BindProperty<bool> BooleanProperty = new BindProperty<bool>(true);
 
             public bool Boolean
             {
-                get => _booleanProperty.Value;
-                set => _booleanProperty.Value = value;
+                get => BooleanProperty.Value;
+                set => BooleanProperty.Value = value;
+            }
+
+            [Bind]
+            public readonly BindProperty<int> IntProperty = new BindProperty<int>(1);
+
+            public int Int32
+            {
+                get => IntProperty.Value;
+                set => IntProperty.Value = value;
             }
         }
     }
