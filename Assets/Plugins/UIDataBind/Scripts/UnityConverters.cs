56cda825522c1c5d6629794c0a37bf88f90890d1
Andrew Salomatin <merlinkolv@gmail.com>
2019-08-20 17:06:11 +0300

    Implemented textures binder

--- Assets/Plugins/UIDataBind/Scripts/UnityConverters.cs
+++ Assets/Plugins/UIDataBind/Scripts/UnityConverters.cs
@@ -9,8 +9,13 @@ namespace Plugins.UIDataBind
         public static void Register()
         {
             var conversionMethods = BindingKernel.Instance.ConversionMethods;
-            conversionMethods.Register<Sprite, string>(Resources.Load<Sprite>,
-                                                       sprite => sprite != null ? sprite.name : string.Empty);
+            conversionMethods.Register<Sprite, string>(Resources.Load<Sprite>,a => a.GetName());
+            conversionMethods.Register<Texture, string>(Resources.Load<Texture>,a => a.GetName());
+            conversionMethods.Register<Texture2D, string>(Resources.Load<Texture2D>,a => a.GetName());
+            conversionMethods.Register<Texture, Texture2D>(a => (Texture) a, a => (Texture2D) a);
         }
+
+        private static string GetName(this Object obj) =>
+            obj != null ? obj.name : string.Empty;
     }
 }
