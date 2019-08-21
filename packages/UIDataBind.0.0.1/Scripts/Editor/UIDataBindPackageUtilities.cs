bae2c14e04bb84060a0162b54bc70790ba220bc0
Andrew Salomatin <merlinkolv@gmail.com>
2019-08-21 15:25:38 +0300

    Implemented UIDataBind examples importer

---
+++ packages/UIDataBind.0.0.1/Scripts/Editor/UIDataBindPackageUtilities.cs
@@ -0,0 +1,32 @@
+using System.IO;
+using System.Linq;
+using UnityEditor;
+
+namespace Plugins.UIDataBind.Editor
+{
+    public static class UIDataBindPackageUtilities
+    {
+        /// <summary>
+        ///
+        /// </summary>
+        [MenuItem("Window/UIDataBind/Import UIDataBind Examples", false, 2050)]
+        public static void ImportExamplesContentMenu()
+        {
+            var packageFullPath = GetPackageFullPath();
+            AssetDatabase.ImportPackage(packageFullPath + "/Package Resources/Examples.unitypackage", true);
+        }
+
+        private static string GetPackageFullPath()
+        {
+            // Check for potential UPM package
+            var packagePath = Path.GetFullPath(@"Packages/");
+            if (!Directory.Exists(packagePath))
+                return null;
+            
+            packagePath = Directory.GetDirectories(packagePath, "UIDataBind*", SearchOption.TopDirectoryOnly)
+                .FirstOrDefault(p=>Directory.Exists(p + "/Editor Resources"));
+
+            return packagePath;
+        }
+    }
+}
